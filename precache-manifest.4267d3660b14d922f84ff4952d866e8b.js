cb2520ac885ae9ca369ea7528e62a973d4c92605
ikescorp <ike.scorp@gmail.com>
2020-04-27 12:00:34 +0000

    Deploying to gh-pages from  @ 4ad6481160628ae18c028ab4a11a985f8d886270 🚀

---
+++ precache-manifest.4267d3660b14d922f84ff4952d866e8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7704a8d32d70e8d1e6f8e1ed5277d85a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b5481e1742429c744d2",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "94153fdc1aa66ddb9b17",
+    "url": "/static/css/main.8b68e7c9.chunk.css"
+  },
+  {
+    "revision": "6b5481e1742429c744d2",
+    "url": "/static/js/2.dda7384d.chunk.js"
+  },
+  {
+    "revision": "577b7a3b9fbfa17fccf2f4ddb5a84180",
+    "url": "/static/js/2.dda7384d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94153fdc1aa66ddb9b17",
+    "url": "/static/js/main.d54e9a79.chunk.js"
+  },
+  {
+    "revision": "20e74bb56b5aad9a2c37",
+    "url": "/static/js/runtime-main.96a74c67.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0171b4dba8d9a4515adbee5229e322c.js"
+  "/precache-manifest.4267d3660b14d922f84ff4952d866e8b.js"
 );
 
 self.addEventListener('message', (event) => {
